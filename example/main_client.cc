23be33cd0b8618af84b766277a24993086ade581
Joseph Atalla <josephataullah@gmail.com>
2022-04-04 13:54:04 +0200

    fixed core dumped when sending not enough args

--- example/main_client.cc
+++ example/main_client.cc
@@ -8,11 +8,12 @@ int main(int argc, char **argv) { //skickar host o port
     //TODO
     if(argc != 3){
         cout << "Not enough arguments" << endl;
-    }
-    char* host = argv[1];
-    int port = stoi(argv[2]);
+    } else {
+        char* host = argv[1];
+        int port = stoi(argv[2]);
 
-    //Connection c(host, port);
-    Client client = Client(std::shared_ptr<Connection>(new Connection(host, port)));
-    client.begin();
+        //Connection c(host, port);
+        Client client = Client(std::shared_ptr<Connection>(new Connection(host, port)));
+        client.begin();
+    }
 }
